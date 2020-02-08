da1afcbeb226bc8ed5bcbff4275e0a3357b11512
TOON <toon7788554@gmail.com>
2020-02-08 15:44:02 +0800

    frame

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="layout" id="app">
+    <Layout>
+      <Header>
+        <MyHeader/>
+      </Header>
+      <Layout :style="{padding: '0 10px'}">
+        <MyBreadcrumb/>
+        <Content :style="{padding: '24px', minHeight: '700px',maxHeight:'800px', background: '#fff'}">
+          <Layout>
+            <Sider hide-trigger :style="{background: '#fff'}">
+                <my-steps/>
+            </Sider>
+              <Layout :style="{padding: '24px 24px'}">
+                  <Content :style="{padding: '24px', minHeight: '700px', background: '#fff'}">
+                      Content
+                  </Content>
+              </Layout>
+          </Layout>
+        </Content>
+      </Layout>
+      <Footer class="layout-footer-center">2019-2020 &copy; ANANASAFT</Footer>
+    </Layout>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/Header.vue'
+import MySteps from './components/Steps'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyHeader,
+    MySteps
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+        font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+    }
+    .layout-footer-center{
+        text-align: center;
+    }
 </style>
---
+++ src/components/Steps.vue
@@ -0,0 +1,21 @@
+<template>
+    <Steps class="steps" :current="2" direction="vertical">
+        <Step title="已完成" content="传感器功能"></Step>
+        <Step title="已完成" content="传感器参数"></Step>
+        <Step title="进行中" content="传感器工作环境"></Step>
+        <Step title="待进行" content="网关参数"></Step>
+        <Step title="待进行" content="网关工作环境"></Step>
+        <Step title="待进行" content="方案选择"></Step>
+    </Steps>
+</template>
+<script>
+    export default {
+        name:"MySteps"
+    }
+</script>
+
+<style scoped>
+    .steps{
+        font-size: 30px;
+    }
+</style>
